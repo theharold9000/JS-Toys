341a5ce86938407db323e085aaab2ecdb9784a08
theharold9000 <harold@justdigitalpeople.com.au>
2017-08-13 22:17:40 +1000

    Getting there

--- ForLoopsExercise.js
+++ ForLoopsExercise.js
@@ -1,11 +1,18 @@
-var text = "This is a text string"
-var myName = "Harold"
+var text = "This exercise is totally done"
+var myName = "harold"
 var hits = []
 
-for(var i = 0; i < text.length; i++) {
-    if(text[i] === myName[0]) {
-        for(var j = i; j < myName.length; j++) {
-            hits.push([j]);
+for(var i = 0; i < text.length; i++); {
+    if(text[i] === "h") {
+        for(var j = i; j < (i + myName.length); j++); {
+            hits.push(text[j]);
         }
     }
+}
+
+if (hits.length === 0) {
+    console.log("Your name wasn't found")
+}
+else {
+    console.log(hits)
 }
