76718b540dce3ab07f59e46edc123610899c1067
theharold9000 <harold@justdigitalpeople.com.au>
2017-08-15 00:43:13 +1000

    Just playing around

---
+++ playingAroundWithLoops.js
@@ -0,0 +1,11 @@
+var count = 10;
+while (count > 0) {
+    for (loops = 0; loops < 10; loops++) {
+        count = (count - 1);
+        var done = false;
+        do {
+            console.log("It's working!");
+        }
+        while(done);
+    }
+}
