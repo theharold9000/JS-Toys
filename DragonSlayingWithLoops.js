2e9508ce9b7dcfffd5635ac731a596c5461aa1e1
theharold9000 <harold@justdigitalpeople.com.au>
2017-08-15 19:14:05 +1000

    Progress!

--- DragonSlayingWithLoops.js
+++ DragonSlayingWithLoops.js
@@ -2,3 +2,13 @@ var slaying = true;
 var youHit = Math.floor(Math.random()*2);
 var damageThisRound = Math.floor(Math.random()*5 + 1);
 var totalDamage = 0;
+
+while (slaying) {
+    if (youHit) {
+        console.log("You hit the dragon!");
+    }
+    else {
+        console.log("The dragon defeated you!");
+    }
+    slaying = false;
+}
