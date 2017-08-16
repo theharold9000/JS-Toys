7239fd4bc4f49f5e05a43a9d896bb5a293442492
theharold9000 <harold@justdigitalpeople.com.au>
2017-08-16 18:22:45 +1000

    Building functionality

--- SimpleContactList.js
+++ SimpleContactList.js
@@ -3,10 +3,30 @@ var friends = {
         firstName: "Bill",
         lastName: "Martin",
         number: "0423949238",
+        address: ["46 Princess Street", "Fairfield", "4103"],
         },
     steve: {
         firstName: "Steve",
         lastName: "Williams",
         number: "0499491857",
-    },
+        address: ["23 Swann Road", "Taringa", "4163"],
+        },
 };
+
+var list = function() {
+    for (var entry in friends) {
+        console.log(entry);
+    }
+}
+
+var search = function(name) {
+    for (var entry in object) {
+        if (friends[entry].firstName === name) {
+            console.log(friends[entry]);
+            return friends[entry];
+        }
+        else {
+            console.log("No matches found");
+        }
+    }
+}
