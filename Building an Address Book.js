1b3e38c02496c3153420bce25572f86a20a5bb56
theharold9000 <harold@justdigitalpeople.com.au>
2017-08-17 22:36:40 +1000

    New Project!

---
+++ Building an Address Book.js
@@ -0,0 +1,46 @@
+var bob = {
+    firstName: "Bob",
+    lastName: "Jones",
+    phoneNumber: "(650) 777-7777",
+    email: "bob.jones@example.com"
+};
+
+var mary = {
+    firstName: "Mary",
+    lastName: "Johnson",
+    phoneNumber: "(650) 888-8888",
+    email: "mary.johnson@example.com"
+};
+
+var contacts = [bob, mary];
+
+function printPerson(person) {
+    console.log(person.firstName + " " + person.lastName);
+}
+
+function list() {
+	var contactsLength = contacts.length;
+	for (var i = 0; i < contactsLength; i++) {
+		printPerson(contacts[i]);
+	}
+}
+
+/*Create a search function
+then call it passing "Jones"*/
+function search(lastName) { 
+    var contactsLength = contacts.length;
+    for (var i = 0; i < contactsLength; i++) {
+        if (contacts[i].lastName === lastName) {
+            printPerson(contacts[i]);
+        }
+    }
+}
+
+function add(firstName, lastName, phoneNumber, email) {
+    var firstName = {
+        firstName = firstName,
+        lastName = lastName,
+        phoneNumber = phoneNumber,
+        email = email,
+    }
+}
