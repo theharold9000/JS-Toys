84a6e8e90b91ff401f8c3e8ea88da1ae7e2cde70
theharold9000 <harold@justdigitalpeople.com.au>
2017-08-26 08:02:02 +1000

    New Project

---
+++ Candidate DB.js
@@ -0,0 +1,7 @@
+var candidate = function(firstName, lastName, techStack, state) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.techStack = techStack;
+    this.state = state;
+}
+
